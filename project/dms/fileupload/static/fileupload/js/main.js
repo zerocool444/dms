ca2b8e300fcbeaf5fc1e109748974fa993b85ddc
James Fourman <zerocooldoh@gmail.com>
2015-06-24 22:20:09 -0400

    Added jquery file upload

---
+++ project/dms/fileupload/static/fileupload/js/main.js
@@ -0,0 +1,50 @@
+/*
+ * jQuery File Upload Plugin JS Example 8.9.1
+ * https://github.com/blueimp/jQuery-File-Upload
+ *
+ * Copyright 2010, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/MIT
+ */
+
+/* global $, window */
+
+$(function () {
+    'use strict';
+
+    // Initialize the jQuery File Upload widget:
+    $('#fileupload').fileupload({
+        // Uncomment the following to send cross-domain cookies:
+        //xhrFields: {withCredentials: true},
+        
+    });
+
+    $("#fileupload")
+        .bind('fileuploadadd', function(e, data){
+            $.each(data.files, function (index, file) {
+                console.log('Added file: ' + file.name);
+            });
+        })
+        .bind('fileuploadchange', function(e, data){
+
+        });
+    
+    // Load existing files:
+    $('#fileupload').addClass('fileupload-processing');
+    $.ajax({
+        // Uncomment the following to send cross-domain cookies:
+        //xhrFields: {withCredentials: true},
+        url: $('#fileupload').fileupload('option', 'url'),
+        dataType: 'json',
+        context: $('#fileupload')[0]
+    }).always(function () {
+        $(this).removeClass('fileupload-processing');
+    }).done(function (result) {
+        $(this).fileupload('option', 'done')
+            .call(this, $.Event('done'), {result: result});
+    });
+    
+
+});
