dc9ea3aa97c9853cb8825bb0ccd51fc0892a7206
James Fourman <zerocooldoh@gmail.com>
2015-06-25 22:19:19 -0400

    Recent Additions
    Added handlebars javascript to run templates, also created the template
    for the upload section

--- project/dms/fileupload/static/fileupload/js/main.js
+++ project/dms/fileupload/static/fileupload/js/main.js
@@ -10,25 +10,36 @@
  */
 
 /* global $, window */
-
 $(function () {
     'use strict';
+
+    var source   = $("#upload-file-template").html();
+    var template = Handlebars.compile(source);
 
     // Initialize the jQuery File Upload widget:
     $('#fileupload').fileupload({
         // Uncomment the following to send cross-domain cookies:
         //xhrFields: {withCredentials: true},
-        
+        dropZone: $(".upload_box"),
+        autoUpload: false
     });
 
     $("#fileupload")
         .bind('fileuploadadd', function(e, data){
             $.each(data.files, function (index, file) {
                 console.log('Added file: ' + file.name);
+                
+                var context = {filename: file.name};
+                $(".upload_area").append(template(context));
+                       
+
             });
         })
         .bind('fileuploadchange', function(e, data){
 
+        })
+        .bind('fileuploadprogress', function (e, data) {
+            var progress = parseInt(data.loaded / data.total * 100, 10);
         });
     
     // Load existing files:
